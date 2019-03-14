796f7e3d5931c1be1b69143a7b910e7309c0f813
Artoria <Artoria>
2019-03-14 20:06:54 +0800

    minor

--- require.pl
+++ require.pl
@@ -1,10 +1,19 @@
-:-module(require, [git/2, git/3, github/1, github/2]).
+:-module(require, [
+    run/1,
+    run/2,
+    git/2, 
+    git/3, 
+    github/1, 
+    github/2]).
 
 mkdir(A) :- exists_directory(A), !; make_directory(A).
 
 run(Fmt, Arg) :-
     format(string(Line), Fmt, Arg),
     shell(Line).
+
+run(Str) :-
+    shell(Str).
 
 git(_, Name) :-
     format(string(Dir), 'git/~s', [Name]),
@@ -24,3 +33,4 @@ git(Header, A, B) :-
 
 github(Name) :- git('https://github.com', Name).
 github(A, B) :- git('https://github.com', A, B).
+
